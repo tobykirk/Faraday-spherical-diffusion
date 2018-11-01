9678bf7373a117d6366ec84866434fd0ae14fe94
DrSOKane <s.okane@imperial.ac.uk>
2018-11-01 16:36:46 +0000

    Updated tests with .any function and added comments

--- general-test-suite.py
+++ general-test-suite.py
@@ -1,3 +1,4 @@
+import numpy as np
 from spherical-diffusion import *
 
 cmax = 12000
@@ -10,13 +11,27 @@ Nr = R/dr
 [cs,complete,errorflag] = spherical-diffusion(j0,c0,R,dr)
 
 def completed():
+    """
+    Tests whether the simulation was completed with no fatal errors.
+    The test passes if the simulation reached the final time step and no error flags were triggered.
+    """
     assert complete == 1
     assert errorflag == 0
 
 def inrange():
-    assert cs[-1,:] > 0
-    assert cs[-1,:] < cmax
+    """
+    Tests whether the concentration profile is in the allowed range.
+    The allowed range is defined as being between 0 and cmax.
+    The test passes if the final concentration is in this range for all r and t.
+    """
+    assert cs.any > 0
+    assert cs.any < cmax
 
 def continuity():
-    dc = cs[-1,0:Nr-1]-cs[-1,1:Nr]
-    assert dc[:] < cmax
+    """
+    Tests whether or not the concentration profile has any discontinuities.
+    A discontinuity is defined here as a finite difference being greater than cmax.
+    The test passes if no such discontinuities are detected.
+    """
+    dc = cs[:,0:Nr-1]-cs[:,1:Nr]
+    assert dc.any < cmax
--- steady-state-test.py
+++ steady-state-test.py
@@ -1,3 +1,4 @@
+import numpy as np
 from spherical-diffusion import *
 
 cmax = 12000
@@ -10,8 +11,16 @@ Nr = R/dr
 [cs,complete,errorflag] = spherical-diffusion(j0,c0,R,dr)
 
 def completed():
+    """
+    Tests whether or not the simulation was completed with no fatal errors.
+    The test passes if the simulation reached the final time step with no error flags triggered.
+    """
     assert complete == 1
     assert errorflag == 0
 
 def steadystate():
-    assert cs[-1,:] == c0
+    """
+    Tests whether the simulation gives the correct answer for the trivial case of zero flux.
+    The test passes if the concentration is equal to c0 everywhere.
+    """
+    assert cs.any == c0
