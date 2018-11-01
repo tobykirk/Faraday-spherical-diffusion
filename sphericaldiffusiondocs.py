1a75cf0fcac1f8493ad7e5d1695e56656f38d217
Rowena Brugge <r.brugge14@imperial.ac.uk>
2018-11-01 15:52:17 +0000

    testdoc

---
+++ sphericaldiffusiondocs.py
@@ -0,0 +1,20 @@
+def squared(x):
+    """
+    Returns the square if the input x
+
+    Longer description - the square is found by mulitplying x by x.
+
+    :param x:
+        base number
+    :return: x*x
+        base number raised to power 2
+
+    Examples
+
+    >>> squared(2)
+    4
+
+    >>>squared(-1)
+    1
+    """
+    return x*x
